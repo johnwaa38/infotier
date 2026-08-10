d1b0ad3d0d37e3730d166c0f0973cbd7787f6200
John J Foxall <johnwaa38@gmail.com>
2026-08-10 06:44:26 -0500

    Secure api-nest/src/app.module.ts

--- api-nest/src/app.module.ts
+++ api-nest/src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { PrismaService } from './common/prisma.service';
 import { SessionsModule } from './sessions/sessions.module';
 import { VerificationsModule } from './verifications/verifications.module';
 import { WebhooksModule } from './webhooks/webhooks.module';
 import { AppController } from './app.controller';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [SessionsModule, VerificationsModule, WebhooksModule],
-  providers: [PrismaService],
+  imports: [AuthModule, SessionsModule, VerificationsModule, WebhooksModule],
+  providers: [PrismaService, { provide: APP_GUARD, useClass: AuthGuard }],
   controllers: [AppController],
 })
 export class AppModule {}
