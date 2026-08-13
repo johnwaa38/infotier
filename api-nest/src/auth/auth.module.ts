fc1127af58071d800c668429f360a950c5a3a5e8
John J Foxall <johnwaa38@gmail.com>
2026-08-13 11:59:21 -0500

    Add customer usage and restore public sessions

--- api-nest/src/auth/auth.module.ts
+++ api-nest/src/auth/auth.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ApiKeysService } from './api-keys.service';
-import { CustomersController } from './customers.controller';
+import { CustomersController, CustomerPortalController } from './customers.controller';
 import { PrismaService } from '../common/prisma.service';
 
-@Module({ controllers: [AuthController, CustomersController], providers: [AuthService, ApiKeysService, PrismaService], exports: [AuthService, ApiKeysService] })
+@Module({ controllers: [AuthController, CustomersController, CustomerPortalController], providers: [AuthService, ApiKeysService, PrismaService], exports: [AuthService, ApiKeysService] })
 export class AuthModule {}
