8d95dd5d7e33163ee2a6ac02b211c0b3552d8714
John J Foxall <johnwaa38@gmail.com>
2026-08-12 19:38:28 -0500

    Fix API-key service dependency wiring

--- api-nest/src/auth/auth.module.ts
+++ api-nest/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ApiKeysService } from './api-keys.service';
 import { CustomersController } from './customers.controller';
+import { PrismaService } from '../common/prisma.service';
 
-@Module({ controllers: [AuthController, CustomersController], providers: [AuthService, ApiKeysService], exports: [AuthService, ApiKeysService] })
+@Module({ controllers: [AuthController, CustomersController], providers: [AuthService, ApiKeysService, PrismaService], exports: [AuthService, ApiKeysService] })
 export class AuthModule {}
