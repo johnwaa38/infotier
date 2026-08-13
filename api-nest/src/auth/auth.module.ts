1c4b8f8f32d712e77d74ee2cca3c92469657b55e
John J Foxall <johnwaa38@gmail.com>
2026-08-12 19:35:53 -0500

    Add customer API-key authentication

--- api-nest/src/auth/auth.module.ts
+++ api-nest/src/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ApiKeysService } from './api-keys.service';
+import { CustomersController } from './customers.controller';
 
-@Module({ controllers: [AuthController], providers: [AuthService], exports: [AuthService] })
+@Module({ controllers: [AuthController, CustomersController], providers: [AuthService, ApiKeysService], exports: [AuthService, ApiKeysService] })
 export class AuthModule {}
