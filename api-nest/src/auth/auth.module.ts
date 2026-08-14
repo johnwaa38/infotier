725df8c0faad8b97859a71e47ce0752aac9c8821
John J Foxall <johnwaa38@gmail.com>
2026-08-14 11:28:20 -0500

    Add free beta onboarding

--- api-nest/src/auth/auth.module.ts
+++ api-nest/src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { ApiKeysService } from './api-keys.service';
 import { CustomersController, CustomerPortalController } from './customers.controller';
 import { PrismaService } from '../common/prisma.service';
+import { SignupRequestsController } from './signup-requests.controller';
 
-@Module({ controllers: [AuthController, CustomersController, CustomerPortalController], providers: [AuthService, ApiKeysService, PrismaService], exports: [AuthService, ApiKeysService] })
+@Module({ controllers: [AuthController, CustomersController, CustomerPortalController, SignupRequestsController], providers: [AuthService, ApiKeysService, PrismaService], exports: [AuthService, ApiKeysService] })
 export class AuthModule {}
